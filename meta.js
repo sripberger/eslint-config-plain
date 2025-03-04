5e93337b2eeeaf77cc6f9deab28018c29a591073
Steve Ripberger <steve.ripberger@gmail.com>
2025-03-04 15:28:32 -0500

    Finish migration to eslint@9

--- meta.js
+++ meta.js
@@ -1,12 +1,17 @@
-module.exports = {
-	extends: "./index.js",
-	rules: {
-		"quote-props": ["error", "consistent-as-needed"],
-		"sort-keys": ["error", "asc", {
-			caseSensitive: false,
-			minKeys: 10,
-			natural: true,
-			allowLineSeparatedGroups: true,
-		}],
+import index from "./index.js";
+
+export default [
+	...index,
+	{
+		name: "@sripberger/eslint-config-plain meta overrides",
+		rules: {
+			"quote-props": ["error", "consistent-as-needed"],
+			"sort-keys": ["error", "asc", {
+				caseSensitive: false,
+				minKeys: 10,
+				natural: true,
+				allowLineSeparatedGroups: true,
+			}],
+		},
 	},
-};
+];
